e2761830bfe42850a0db29ae1f885b5ab678589e
creator54 <hi.creator54@gmail.com>
2023-01-29 23:47:18 +0530

    add chatgpt plugin

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,4 +1,18 @@
 return {
+  { "navarasu/onedark.nvim" },
+  {
+    "jackMort/ChatGPT.nvim",
+    config = function()
+    require("chatgpt").setup({
+    -- optional configuration
+    })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
