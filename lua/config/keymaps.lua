6da776551ffd6ee520db6a828ff44b60fd822981
creator54 <hi.creator54@gmail.com>
2023-02-10 16:48:38 +0530

    add keybinds: terminal, Lazy update & Lazy sync

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n","<A-u>","<cmd>:Lazy update<cr>", { desc = "Lazy update" })
+vim.keymap.set("n","<A-S>","<cmd>:Lazy sync<cr>", { desc = "Lazy update" })
+vim.keymap.set("n","<A-t>","<cmd>:terminal<cr>i", { desc = "Open terminal buffer" })
