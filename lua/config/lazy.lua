cda5d5b2d76090253c3204363211b4084a2303c8
creator54 <hi.creator54@gmail.com>
2023-07-31 13:41:23 +0530

    disable auto plugins update check

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
+    -- automatically check for plugin updates
     enabled = false,
     concurrency = nil, ---@type number? set to 1 to check for updates very slowly
     notify = false, -- get a notification when new updates are found
