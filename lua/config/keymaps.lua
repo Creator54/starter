a8687e61ed13d46daf2f494d1d89b6648c4b2b52
creator54 <hi.creator54@gmail.com>
2024-04-20 23:38:48 +0530

    switch from glow.nvim to glow cli in vim buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,7 +5,14 @@
 vim.keymap.set("n", "<A-u>", "<cmd>:Lazy update<cr>", { desc = "Lazy update" })
 vim.keymap.set("n", "<A-S>", "<cmd>:Lazy sync<cr>", { desc = "Lazy update" })
 vim.keymap.set("n", "<A-t>", "<cmd>:terminal<cr>i", { desc = "Open terminal buffer" })
-vim.keymap.set("n", "<A-r>", "<cmd>:Glow<cr>", { desc = "Open Glow with current file" })
 vim.keymap.set("n", "<A-`>", "<cmd>:BufferLineCycleNext<cr>", { desc = "Cycle forwardthrough open buffers" })
 vim.keymap.set("n", "<A-S-`>", "<cmd>:BufferLineCyclePrev<cr>", { desc = "Cycle backword through open buffers" })
 vim.keymap.set("n", "<C-d>", ":bd<CR>", { desc = "Close current buffer", noremap = true, silent = true })
+
+-- vim.keymap.set("n", "<A-r>", "<cmd>:Glow<cr>", { desc = "Open Glow with current file" })
+vim.api.nvim_set_keymap(
+  "n",
+  "<A-r>",
+  ":vsplit<CR>:lua vim.cmd('term glow ' .. vim.fn.expand('%:p') .. ' -p')<CR>:setlocal nonumber norelativenumber<CR>i<CR>",
+  { desc = "Open Glow with current file in vertical split without line numbers", silent = true }
+)
