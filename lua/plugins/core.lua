1b564a1a7d2af59bd5340f020088a182e8c70cbe
creator54 <hi.creator54@gmail.com>
2026-05-15 22:49:36 +0530

    feat: remove chatgpt plugin

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -16,18 +16,6 @@ return {
     end,
   },
   {
-    "jackMort/ChatGPT.nvim",
-    config = function()
-      require("chatgpt").setup({
-      })
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-  },
-  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "onedark",
