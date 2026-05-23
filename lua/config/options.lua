15282927add8139af5dcf4ee047c522dc87a1d5f
creator54 <hi.creator54@gmail.com>
2026-05-23 19:52:51 +0530

    perf: strip UI bloat for faster navigation

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,11 @@
 
 vim.opt.winblend = 0
 vim.opt.pumblend = 0
+
+-- Performance optimizations for large files and tmux compatibility
+vim.g.loaded_matchparen = 1
+vim.opt.cursorline = false
+vim.opt.relativenumber = false
+vim.opt.number = true
+vim.opt.foldmethod = "manual"
+vim.opt.foldenable = false
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,10 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      opts.highlight = opts.highlight or {}
-      opts.highlight.disable = function(lang, buf)
-        return lang == "neo-tree" or vim.bo[buf].filetype == "neo-tree"
-      end
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    enabled = false,
+    opts = {
+      highlight = {
+        enable = true,
+      },
+    },
   },
 }
